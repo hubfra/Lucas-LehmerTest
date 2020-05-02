d4477eceb0fbe1a178de9a99c4d348f91642394d
hubfra <51798356+hubfra@users.noreply.github.com>
2020-05-02 10:57:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 
 #include <iostream>
-#include <vector>
 
-using namespace std;
 
 bool LucasLehmerTest(int p);
 
-
 int main()
 {
-	vector<int> primes{ 3,5,7,11,13,17,19,23,29,31 };
+	int primes[]{ 3,5,7,11,13,17,19,23,29,31,34 };
 	
 	for (int x : primes) {
-		cout << "2^" << x << " - 1 = " << (1ll << x) - 1 << " is " << (LucasLehmerTest(x) ? "prime\n" : "composite\n");
+		std::cout << "2^" << x << " - 1 = " << (1ll << x) - 1ll << " is " << (LucasLehmerTest(x) ? "prime\n" : "composite\n");
 	}
 
 	return 0;
@@ -20,11 +17,12 @@ int main()
 
 bool LucasLehmerTest(int p)
 {
-	long long m = (1ll << p) - 1; // Mersenne number
-	long long s = 4;              // Last element in the sequence
+	long long m = (1ll << p) - 1ll;
+	long long s = 4ll;
 
-	for (int a = 0; a < p - 2; a++)
+	for (int a = 0; a < p - 2; a++) {
 		s = (s * s - 2) % m;
+	}
 
 	return s == 0;
 }
